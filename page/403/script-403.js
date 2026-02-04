22d3f9eb6463ce32c2d1d8f9fccfb8fb863625dc
neo <neo@xtechgroup.io>
2026-02-04 16:12:40 +0700

    add 403 page

--- page/403/script-403.js
+++ page/403/script-403.js
@@ -1,13 +1,17 @@
 (function() {
     const translations = {
-        // 'zh': {
-        //     title: '访问受限',
-        //     msg: '您当前所在的国家/地区无法访问此网站。如果您认为这是一个错误，请通过以下方式联系支持人员：'
-        // },
-        // 'vi': {
-        //     title: 'Truy cập bị hạn chế',
-        //     msg: 'Truy cập vào trang web này bị hạn chế ở quốc gia hiện tại của bạn. Nếu bạn cho rằng đây là một lỗi, vui lòng liên hệ bộ phận hỗ trợ qua '
-        // },
+        'en': {
+            title: 'Access Restricted',
+            msg: 'Access to this site is restricted in your current country. If you think this is a mistake, please contact support via '
+        },
+        'zh': {
+            title: '访问受限',
+            msg: '您当前所在的国家/地区无法访问此网站。如果您认为这是一个错误，请通过以下方式联系支持人员：'
+        },
+        'vi': {
+            title: 'Truy cập bị hạn chế',
+            msg: 'Truy cập vào trang web này bị hạn chế ở quốc gia hiện tại của bạn. Nếu bạn cho rằng đây là một lỗi, vui lòng liên hệ bộ phận hỗ trợ qua '
+        },
         'es': {
             title: 'Acceso Restringido',
             msg: 'El acceso a este sitio está restringido en su país actual. Si cree que esto es un error, contacte al soporte en '
